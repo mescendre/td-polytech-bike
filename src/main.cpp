735787ea6c138c7a956aa2d8e513c56c021b5e43
antoine jammes <antoine.jammes@etud.univ-angers.fr>
2021-01-19 08:55:28 +0100

    modification_pour_ajouter_une_route

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 
 int main()
 {
-    Road road(100);
+    Road road(20);
     //road.show();
     Bike bike;
     bike.run(road);
+    return 0;
+    
+    Road longRoad(40);
+    bire.run(longroas);
     return 0;
 }
